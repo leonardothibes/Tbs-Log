502a0f81bb5f5005d90179121688e41caf3dc9f8
Leonardo Thibes <leonardothibes@gmail.com>
2014-10-03 22:50:52 -0300

    updating php include_path on tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,11 +4,7 @@ define('LIBRARY_PATH', dirname(__FILE__) . '/../src');
 define('LOGS_PATH', dirname(__FILE__) . '/../logs');
 define('STUFF_PATH', dirname(__FILE__) . '/Tbs/.stuff');
 
-set_include_path(implode(PATH_SEPARATOR, array(
-	LIBRARY_PATH,
-	//get_include_path(),
-)));
-
+set_include_path(LIBRARY_PATH);
 require_once LIBRARY_PATH . '/../vendor/autoload.php';
 
 $logfile = LOGS_PATH . '/debug_' . date('Y-m-d') . '.log';
